b0157eb4ee3db2dc03c0def3b2292aa784ff2267
Jay <jaypatel01122000@gmail.com>
2023-02-25 12:14:58 -0400

    Updated import paths

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-import Register from './components/Register';
-import Login from './components/Login';
-import Home from './components/Home';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import Layout from './components/Layout';
 import Editor from './components/Editor';
-import Admin from './components/Admin';
-import Missing from './components/Missing';
-import Unauthorized from './components/Unauthorized';
-import Lounge from './components/Lounge';
-import LinkPage from './components/LinkPage';
+import Admin from './components/Admin/Admin';
+import Missing from './components/Missing/Missing';
+import Unauthorized from './components/Unauthorized/Unauthorized';
+import Lounge from './components/Lounge/Lounge';
+import LinkPage from './components/Linkspage/LinkPage';
 import RequireAuth from './components/RequireAuth';
 import { Routes, Route } from 'react-router-dom';
 
