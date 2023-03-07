c6c220ee15e9b1eb37be1f3570de388d29a39015
Jay <jaypatel01122000@gmail.com>
2023-03-07 19:09:13 -0400

    Updated Task model which is now compatible with Calendar

--- server/model/Task.js
+++ server/model/Task.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-    username: {
+    title: {
         type: String,
         required: true
     },
-    taskname: {
+    allDay: {
+        type: Boolean,
+        default: true
+    },
+    start: {
+        type: String,
+        required: true
+    }, 
+    end: {
         type: String,
         required: true
     }
