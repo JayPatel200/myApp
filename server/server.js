c3ef83b8aed5bbed020d92e2a1f077b3898be0a4
Jay <jaypatel01122000@gmail.com>
2023-03-21 13:51:31 -0300

    Added socket.io server to recieve, save, and send messages to/from chat

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const verifyJWT = require('./middleware/verifyJWT');
-const cookieParser = require('cookie-parser');
-const credentials = require('./middleware/credentials');
-const mongoose = require('mongoose');
-const connectDB = require('./config/dbConn');
+const appChat = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
+const http = require("http");
+const { Server } = require("socket.io");
+const messagesController = require("./controllers/messagesController");
+const Message = require("../server/model/Message");
 
 // Connect to MongoDB
 connectDB();
@@ -29,41 +34,84 @@ app.use(cors(corsOptions));
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
 
 //middleware for cookies
 app.use(cookieParser());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+//seperate server for chat through socket.io
+const server = http.createServer(appChat);
+
+let interval;
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("callback", async (data) => {
+    try {
+        const messages = await Message.find();
+        socket.emit("receive_message", messages);
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+  socket.on("disconnect", () => {
+    clearInterval(interval);
+    console.log("User Disconnected", socket.id);
+  });
+
+  socket.on("send_message", (data) => {
+    messagesController.saveNewMessage(data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server for chat running on port 3001");
+});
 
 // routes
-app.use('/', require('./routes/root'));
-app.use('/register', require('./routes/register'));
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
+app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+app.use("/messages", require("./routes/api/messages"));
 
 app.use(verifyJWT);
-app.use('/tenants', require('./routes/api/tenants'));
-app.use('/users', require('./routes/api/users'));
-app.use('/tasks', require('./routes/api/tasks'));
+app.use("/tenants", require("./routes/api/tenants"));
+app.use("/users", require("./routes/api/users"));
+app.use("/tasks", require("./routes/api/tasks"));
 
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ "error": "404 Not Found" });
-    } else {
-        res.type('txt').send("404 Not Found");
-    }
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
 
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
