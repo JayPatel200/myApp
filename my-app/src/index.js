6aac4b38c2a75f7ad5890d268304de2051f34309
Jay <jaypatel01122000@gmail.com>
2023-03-21 13:32:09 -0300

    Wrapped inside NavProvider context

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AuthProvider } from "./context/AuthProvider";
+import { NavProvider } from "./context/NavProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
+        <NavProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </NavProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
