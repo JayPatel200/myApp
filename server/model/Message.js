c412c815edda527f4d32e795e102bddfc0696fef
Jay <jaypatel01122000@gmail.com>
2023-03-21 13:53:33 -0300

    MongoDB model for messages

---
+++ server/model/Message.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  room: { 
+    type: String, 
+    required: true 
+},
+  author: { 
+    type: String, 
+    required: true 
+},
+  message: { 
+    type: String, 
+    required: true 
+},
+  time: { 
+    type: String, 
+    required: true 
+}
+});
+
+module.exports = mongoose.model("Message", messageSchema);
