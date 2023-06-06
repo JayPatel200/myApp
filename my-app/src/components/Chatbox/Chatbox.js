d32cc5f88afbf011db79b71146e843696d29afc9
Jay <jaypatel01122000@gmail.com>
2023-06-05 22:33:13 -0300

    component that calls chat component to open respective chats for each users

--- my-app/src/components/Chatbox/Chatbox.js
+++ my-app/src/components/Chatbox/Chatbox.js
@@ -1,23 +1,27 @@
 import "./Chatbox.css";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Chat from "../Chat/Chat";
 import Nav from "../Nav/Nav";
-import useAuth from '../../hooks/useAuth';
+import useAuth from "../../hooks/useAuth";
 
 const socket = io.connect("http://localhost:3001");
 
 const Chatbox = () => {
-  const [username, setUsername] = useState("");
   const { auth } = useAuth();
 
   useEffect(() => {
-    socket.emit("join_room", process.env.REACT_APP_CHATROOM_SECRET);
+    socket.emit("join_room", auth.house);
   }, []);
 
   return (
     <div className="Chatbox">
-      <Chat socket={socket} room={process.env.REACT_APP_CHATROOM_SECRET} username={auth.user} />
+      <Chat
+        socket={socket}
+        room={auth.house}
+        username={auth.user}
+        house={auth.house}
+      />
       <Nav />
     </div>
   );
