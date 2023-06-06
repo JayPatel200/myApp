6edaf19fb71647631c034483b75d2f3b9a3573cb
Jay <jaypatel01122000@gmail.com>
2023-06-06 14:52:15 -0300

    Removed unwanted files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,8 @@ import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import Layout from './components/Layout';
-import Editor from './components/Editor';
-import Admin from './components/Admin/Admin';
 import Missing from './components/Missing/Missing';
 import Unauthorized from './components/Unauthorized/Unauthorized';
-import LinkPage from './components/Linkspage/LinkPage';
 import Schedule from './components/Schedule/Schedule';
 import Chatbox from './components/Chatbox/Chatbox';
 import Profile from './components/Profile/Profile';
@@ -22,7 +19,6 @@ function App() {
         {/* public routes */}
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="linkpage" element={<LinkPage />} />
         <Route path="unauthorized" element={<Unauthorized />} />
         <Route path="registerowner" element={<RegisterOwnner />} />
 
@@ -41,14 +37,6 @@ function App() {
 
         <Route element={<RequireAuth allowedRoles={[2001]} />}>
           <Route path="profile" element={<Profile />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[1984]} />}>
-          <Route path="editor" element={<Editor />} />
-        </Route>
-
-        <Route element={<RequireAuth allowedRoles={[5150]} />}>
-          <Route path="admin" element={<Admin />} />
         </Route>
 
         {/* <Route element={<RequireAuth allowedRoles={[1984, 5150]} />}>
